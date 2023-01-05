551de109bb66f4b5c8c3999fa2a335a440d4e983
Elena <keaya@rambler.ru>
2023-01-05 18:41:00 +0300

    Семинар ООП. Абстракция, инкапсуляция, наследование, полиморфизм. пример с животными

---
+++ Animal.java
@@ -0,0 +1,32 @@
+import java.io.ObjectInputStream.GetField;
+
+public class Animal {
+    private String model;
+    private int age;
+
+    public   Animal(String model){
+        this.model = model; 
+    }
+    public Animal(){
+    };
+    public void voice(){
+        System.out.println("Животное говорит");
+    }
+
+    public String getModel() {
+        return model;
+    }
+    public void setModel(String model) {
+        this.model = model;
+    }
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+/*
+ * Абстракция 
+ * Инкапсуляция
+ */
---
+++ Main.java
@@ -0,0 +1,17 @@
+public class Main {
+    public static void main(String[] args) {
+        Animal animal = new Animal();
+        System.out.println(animal.getModel());
+        animal.setModel("Млекопитающее");
+
+        Dog dog = new Dog("Млекопитающее", "Бобик");
+        dog.getModel();
+        dog.setModel("Дворняшка");
+        
+        dog.voice();
+        animal.voice();
+        Animal dog1 = new Dog("Дворняшка", "Бобик");
+        dog.voice();
+    }
+
+}
