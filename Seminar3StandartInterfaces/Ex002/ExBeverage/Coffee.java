e8d464737f603703f93d378d3db22259d795639e
ElenaFat918 <109763007+ElenaFat918@users.noreply.github.com>
2023-01-07 19:42:37 +0300

    Delete Seminar3StandartInterfaces directory

--- Seminar3StandartInterfaces/Ex002/ExBeverage/Coffee.java
+++
@@ -1,5 +0,0 @@
-package Ex002.ExBeverage;
-
-public class Coffee extends Beverage {//конкрет напиток, можно ничего не указывать, тк уже указано в юазовом класса
-    
-}
--- Seminar3StandartInterfaces/Ex002/ExBeverage/Вeans.java
+++
@@ -1,8 +0,0 @@
-package Ex002.ExBeverage;
-
-public class Вeans extends Ingredient {// отдельные ингридиенты
-
-    public Вeans(String brand) {
-        super(brand);
-    }    
-}
